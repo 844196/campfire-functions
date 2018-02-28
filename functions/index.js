0693e11441a51baa3d5d6aff25e6862cce6cd118
Masaya Takeda <844196@gmail.com>
2018-02-28 19:09:00 +0900

    yamano susume

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,19 @@
 const functions = require('firebase-functions');
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+exports.helloWorld = functions.https.onRequest((req, res) => {
+ res.send('Hello from Firebase!');
+});
+
+exports.yamanosusume = functions.https.onRequest((req, res) => {
+  const content = `<!DOCTYPE html>
+<html>
+  <head>
+    <title>yamanosusume</title>
+  </head>
+  <body>
+    <img src="https://i.ytimg.com/vi/bu1-Gajd7Sw/maxresdefault.jpg">
+  </body>
+</html>`;
+
+  res.status(200).send(content);
 });
